d309b4ce288c94620946ff21797540c0d15bae41
LizandroV <61571830+LizandroV@users.noreply.github.com>
2024-08-08 21:12:39 -0500

    adding CSS to the page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    //HAMBUERGER MENU//
+    //HAMBURGER MENU//
     const burger = document.querySelector(".burger");
     const navLinks = document.querySelector(".nav-links");
 
